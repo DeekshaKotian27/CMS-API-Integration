2cdf76d23fe235335b1fea4824f126f3194d0297
DeekshaKotian27 <deekshakotian271@gmail.com>
2025-07-17 18:41:22 +0530

    api code refactoring

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.Repository.Interface;
+using API.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,13 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowFromSpecificOrigin", builder =>
+    {
+        builder.WithOrigins("https://localhost:5000").AllowAnyHeader().AllowAnyMethod();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ProductDBContext>(
     options=>options.UseSqlServer(
         builder.Configuration.GetConnectionString("ProductCMSDB")));
-
+builder.Services.AddTransient<IProductRepository,ProductRepository>();
 
 var app = builder.Build();
 
@@ -24,6 +32,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFromSpecificOrigin");
 
 app.UseAuthorization();
 
---
+++ API/Repository/Interface/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.DTO;
+using API.Model;
+
+namespace API.Repository.Interface
+{
+    public interface IProductRepository
+    {
+        Task<APIResponse> GetProducts();
+        Task<APIResponse> GetProductByID(int id);
+        Task<APIResponse> CreateProduct(ProductDTO product);
+        Task<APIResponse> DeleteProduct(int id);
+        Task<APIResponse> UpdateProduct(int id,ProductDTO productDTO);
+    }
+}
